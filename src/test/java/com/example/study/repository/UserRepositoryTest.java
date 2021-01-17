f3d7126cbe8b6c15c775204af0b3a84664fc84a6
sunny <parks38@uw.edu>
2021-01-18 00:44:50 +0900

    update repositpory, entity VO

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+    // private UserRepository userRepository = new UserRepository();
+
+    public void create() {
+
+    }
+
+    public void read() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+}
